ff2d13b109d0e383d7f1baff471673da321a93e0
Silvo Sposetti <SilvoSposetti@users.noreply.github.com>
2024-11-24 12:38:34 +0100

    Restrict ArrayUtilities to only work with integral types

--- src/ku/utilities/ArrayUtilities.hpp
+++ src/ku/utilities/ArrayUtilities.hpp
@@ -9,13 +9,13 @@
 namespace ArrayUtilities {
 
 /** Helper to construct and initialize a 2D array with some default value
- * @tparam T The type of all elements in the array
- * @tparam N the amount of columns in the array
- * @tparam M the amount of rows in the array
+ * @tparam T The integral type of all elements in the 2D array
+ * @tparam N the amount of columns in the 2D array
+ * @tparam M the amount of rows in the 2D array
  * @param defaultValue The default value for all elements in the array
  * @return A 2D array whose all elemnts are initialized to the same default value
  */
-template <typename T, std::size_t N, std::size_t M>
+template <std::integral T, std::size_t N, std::size_t M>
 static constexpr std::array<std::array<T, N>, M> create2DArray(const T& defaultValue) {
   auto matrix = std::array<std::array<T, N>, M>();
   for (auto& row : matrix) {
@@ -25,11 +25,12 @@ static constexpr std::array<std::array<T, N>, M> create2DArray(const T& defaultV
 }
 
 /** Helper to construct an array filled by the iota function
+ * @tparam T The integral type of all elments in the array
  * @tparam N the amount of elements required in the array
  * @param start The start for the iota function
  * @return An array with size N filled by the iota function
  */
-template <typename T, std::size_t N>
+template <std::integral T, std::size_t N>
 static constexpr std::array<T, N> createIotaArray(const T& start) {
   auto array = std::array<T, N>();
   std::ranges::iota(array, start);
