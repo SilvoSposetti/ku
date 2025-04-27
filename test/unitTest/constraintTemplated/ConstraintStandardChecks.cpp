8eecd7323441980600d57f732a280c1af4fe44dc
Silvo Sposetti <SilvoSposetti@users.noreply.github.com>
2025-04-27 21:38:46 +0200

    Declare templated test invocations with preprocessor macros

--- test/unitTest/constraintTemplated/ConstraintStandardChecks.cpp
+++ test/unitTest/constraintTemplated/ConstraintStandardChecks.cpp
@@ -6,16 +6,27 @@
 #include <algorithm>
 #include <doctest.h>
 
-template <PuzzleIntrinsics intrinsics>
-bool checkItems(ItemsList<intrinsics> items, int32_t itemsAmount) {
+// Define test case template invocations as a macro. This is specifically for "square" PuzzleSpaces
+#define INVOKE_SQUARE_TEST_CASES_FOR_CONSTRAINT(ConstraintType)                                                        \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{0, 0, 0}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{1, 1, 1}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{2, 2, 2}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{3, 3, 3}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{4, 4, 4}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{5, 5, 5}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{6, 6, 6}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{7, 7, 7}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{8, 8, 8}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{9, 9, 9}>{}>);                                    \
+  TEST_CASE_TEMPLATE_INVOKE(checks, ConstraintType<PuzzleIntrinsics<{10, 10, 10}>{}>);
+
+bool checkItems(const auto& items, int32_t itemsAmount) {
   if (itemsAmount == 0) { // If items are not defined, all options must be empty
     return std::ranges::all_of(items, [&](const auto& option) { return option.empty(); });
   } else { // If any item is defined, all of them must be covered
-    // Expecte
     auto expected = std::vector<int32_t>(itemsAmount, 0);
     std::ranges::iota(expected, 0);
     const auto expectedSet = std::unordered_set(expected.begin(), expected.end());
-
     std::vector<int32_t> set;
     for (const auto& option : items) {
       for (const auto& element : option) {
@@ -27,8 +38,6 @@ bool checkItems(ItemsList<intrinsics> items, int32_t itemsAmount) {
 }
 
 TEST_SUITE("ConstraintTemplated: Standard Checks") {
-
-  constexpr auto puzzleIntrinsics = PuzzleIntrinsics<{9, 9, 9}>();
 
   TEST_CASE_TEMPLATE_DEFINE("Standard Checks ", T, checks) {
     // TODO: make this constexpr -> ItemsList should not contain std::vectors
@@ -42,16 +51,16 @@ TEST_SUITE("ConstraintTemplated: Standard Checks") {
     }
 
     SUBCASE("Primary items") {
-      CHECK(checkItems<puzzleIntrinsics>(constraint.primaryItems, primaryItemsAmount));
+      CHECK(checkItems(constraint.primaryItems, primaryItemsAmount));
     }
 
     SUBCASE("Secondary items") {
-      CHECK(checkItems<puzzleIntrinsics>(constraint.secondaryItems, secondaryItemsAmount));
+      CHECK(checkItems(constraint.secondaryItems, secondaryItemsAmount));
     }
   }
 
-  // Invocations
-  TEST_CASE_TEMPLATE_INVOKE(checks, CellConstraint<puzzleIntrinsics>);
-  TEST_CASE_TEMPLATE_INVOKE(checks, RowConstraint<puzzleIntrinsics>);
-  TEST_CASE_TEMPLATE_INVOKE(checks, ColumnConstraint<puzzleIntrinsics>);
+  // Add further constraints here
+  INVOKE_SQUARE_TEST_CASES_FOR_CONSTRAINT(CellConstraint);
+  INVOKE_SQUARE_TEST_CASES_FOR_CONSTRAINT(RowConstraint);
+  INVOKE_SQUARE_TEST_CASES_FOR_CONSTRAINT(ColumnConstraint);
 }
