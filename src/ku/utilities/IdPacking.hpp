929b03c9fd6bc2e4186800d4915a9597ee134af9
Silvo Sposetti <SilvoSposetti@users.noreply.github.com>
2025-05-10 09:55:43 +0200

    Make IdPacking utility constexpr

--- src/ku/utilities/IdPacking.hpp
+++ src/ku/utilities/IdPacking.hpp
@@ -12,7 +12,9 @@ namespace IdPacking {
  * @param yAmount The amount of IDs in the y-dimension
  * @return The packed IDs
  */
-uint32_t packId(uint32_t x, uint32_t y, uint32_t xAmount, uint32_t yAmount);
+constexpr uint32_t packId(uint32_t x, uint32_t y, [[maybe_unused]] uint32_t xAmount, uint32_t yAmount) {
+  return x * yAmount + y;
+}
 
 /** Linearize three positive integer IDs into a single positive integer.
  * @param x The ID in the x dimension
@@ -23,7 +25,10 @@ uint32_t packId(uint32_t x, uint32_t y, uint32_t xAmount, uint32_t yAmount);
  * @param zAmount The amount of IDs in the z-dimension
  * @return The packed IDs
  */
-uint32_t packId(uint32_t x, uint32_t y, uint32_t z, uint32_t xAmount, uint32_t yAmount, uint32_t zAmount);
+constexpr uint32_t
+packId(uint32_t x, uint32_t y, uint32_t z, [[maybe_unused]] uint32_t xAmount, uint32_t yAmount, uint32_t zAmount) {
+  return x * (yAmount * zAmount) + y * (zAmount) + z;
+}
 
 /** Unpacks a linearized positive integer ID into its two components
  * @param packedId The ID to unpack
@@ -31,7 +36,13 @@ uint32_t packId(uint32_t x, uint32_t y, uint32_t z, uint32_t xAmount, uint32_t y
  * @param yAmount The amount of IDs in the y-dimension
  * @return The unpacked values [x, y]
  */
-std::pair<uint32_t, uint32_t> unpackId(uint32_t packedId, uint32_t xAmount, uint32_t yAmount);
+constexpr std::pair<uint32_t, uint32_t>
+unpackId(uint32_t packedId, [[maybe_unused]] uint32_t xAmount, uint32_t yAmount) {
+  return {
+      packedId / yAmount,
+      packedId % yAmount,
+  };
+}
 
 /** Unpacks a linearized positive integer ID into its three components
  * @param packedId The ID to unpack
@@ -40,7 +51,13 @@ std::pair<uint32_t, uint32_t> unpackId(uint32_t packedId, uint32_t xAmount, uint
  * @param yAmount The amount of IDs in the z-dimension
  * @return The unpacked values [x, y, z]
  */
-std::tuple<uint32_t, uint32_t, uint32_t>
-unpackId(uint32_t packedId, uint32_t xAmount, uint32_t yAmount, uint32_t zAmount);
+constexpr std::tuple<uint32_t, uint32_t, uint32_t>
+unpackId(uint32_t packedId, [[maybe_unused]] uint32_t xAmount, uint32_t yAmount, uint32_t zAmount) {
+  return {
+      packedId / (yAmount * zAmount), // 0 <= x < xAmount
+      (packedId % (yAmount * zAmount)) / zAmount, //  0 <= y < yAmount
+      packedId % zAmount, //  0 <= z < zAmounts
+  };
+}
 
 } // namespace IdPacking
