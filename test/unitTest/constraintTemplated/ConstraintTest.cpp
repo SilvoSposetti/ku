565f560e94052ae0a94b46d43d73ba4550fa8a23
Silvo Sposetti <SilvoSposetti@users.noreply.github.com>
2025-05-23 18:43:12 +0200

    Rewrite example constraint

--- test/unitTest/constraintTemplated/ConstraintTest.cpp
+++ test/unitTest/constraintTemplated/ConstraintTest.cpp
@@ -2,37 +2,56 @@
 #include "constraintTemplated/Constraint.hpp"
 
 #include "ConstraintTestHelpers.hpp"
+#include "constraintTemplated/ConstraintConcept.hpp"
 #include "utilities/IdPacking.hpp"
 
 #include <doctest.h>
 
-template <PuzzleIntrinsics puzzleIntrinsics>
-struct ExampleConstraint {
+namespace ExampleConstraintOptionSizes {
+static constexpr std::size_t primary = 1;
+static constexpr std::size_t secondary = 1;
+} // namespace ExampleConstraintOptionSizes
+
+template <PuzzleIntrinsics puzzle>
+struct ExampleConstraint : public Constraint<ExampleConstraint<puzzle>,
+                                             puzzle,
+                                             ExampleConstraintOptionSizes::primary,
+                                             ExampleConstraintOptionSizes::secondary> {
+public:
+  constexpr ExampleConstraint()
+      : Constraint<ExampleConstraint<puzzle>,
+                   puzzle,
+                   ExampleConstraintOptionSizes::primary,
+                   ExampleConstraintOptionSizes::secondary>(
+            ConstraintType::NONE, "Name", "Description") {
+    static_assert(ConstraintConcept<ExampleConstraint,
+                                    puzzle,
+                                    ExampleConstraintOptionSizes::primary,
+                                    ExampleConstraintOptionSizes::secondary>,
+                  "CellConstraint does not satisfy ConstraintConcept");
+  };
 
   constexpr static bool supportsPuzzle() {
     return true;
   }
 
-  constexpr static Option<1> primaryOption(Index row, Index column, Digit digit) {
-    return Option<1>{static_cast<OptionId>(IdPacking::packId(
-        row, column, digit - 1, puzzleIntrinsics.rows, puzzleIntrinsics.columns, puzzleIntrinsics.digits.size()))};
+  constexpr static Option<ExampleConstraintOptionSizes::primary> primaryOption(Index row, Index column, Digit digit) {
+    return Option<1>{static_cast<OptionId>(
+        IdPacking::packId(row, column, digit - 1, puzzle.rows, puzzle.columns, puzzle.digits.size()))};
   }
 
-  constexpr static Option<1> secondaryOption(Index row, Index column, Digit digit) {
-    constexpr auto total =
-        static_cast<uint32_t>(puzzleIntrinsics.rows * puzzleIntrinsics.columns * puzzleIntrinsics.digits.size());
-    return Option<1>{static_cast<OptionId>(
-        (total - 1) -
-        IdPacking::packId(
-            row, column, digit - 1, puzzleIntrinsics.rows, puzzleIntrinsics.columns, puzzleIntrinsics.digits.size()))};
+  constexpr static Option<ExampleConstraintOptionSizes::secondary>
+  secondaryOption(Index row, Index column, Digit digit) {
+    constexpr auto total = static_cast<uint32_t>(puzzle.rows * puzzle.columns * puzzle.digits.size());
+    return {static_cast<OptionId>(
+        (total - 1) - IdPacking::packId(row, column, digit - 1, puzzle.rows, puzzle.columns, puzzle.digits.size()))};
   }
 };
 
 TEST_CASE("Base Constraint") {
 
   constexpr auto puzzleIntrinsics = PuzzleIntrinsics<{2, 2, 2}>();
-  constexpr auto constraint = Constraint<ExampleConstraint<puzzleIntrinsics>, puzzleIntrinsics, 1, 1>(
-      ConstraintType::NONE, "Name", "Description");
+  constexpr auto constraint = ExampleConstraint<puzzleIntrinsics>();
 
   CHECK_EQ(constraint.type, ConstraintType::NONE);
   CHECK_EQ(constraint.name, "Name");
