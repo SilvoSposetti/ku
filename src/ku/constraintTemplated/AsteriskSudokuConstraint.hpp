2054b7e84b438a20f3d912dbc01eefa7a4105a94
Silvo Sposetti <SilvoSposetti@users.noreply.github.com>
2025-05-23 16:07:51 +0200

    Use ranges::any_of since ranges::contains can't find a proper comparison on the runner

--- src/ku/constraintTemplated/AsteriskSudokuConstraint.hpp
+++ src/ku/constraintTemplated/AsteriskSudokuConstraint.hpp
@@ -38,7 +38,7 @@ public:
   primaryOption(uint32_t row, uint32_t column, uint32_t digit) {
     constexpr std::array<std::pair<Index, Index>, 9> cells = {
         std::make_pair(1, 4), {2, 2}, {2, 6}, {4, 1}, {4, 4}, {4, 7}, {6, 2}, {6, 6}, {7, 4}};
-    if (std::ranges::contains(cells, std::make_pair(row, column))) {
+    if (std::ranges::any_of(cells, [&](const auto& pair) { return pair.first == row && pair.second == column; })) {
       return {static_cast<OptionId>(digit - 1)};
     }
     return {};
